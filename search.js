bb3a82e136c3cd7c661c1694aa2fd3cae5978efb
Andrew Hansen <123025570+HansenSAndrew@users.noreply.github.com>
2023-04-29 19:20:21 -0400

    Work on presentation of results

--- search.js
+++ search.js
@@ -6,17 +6,85 @@ window.addEventListener('load', function () {
     const button = document.getElementById('searchButton');
     console.log(button);
 
+
     // Book Information
-    let book = document.createElement('div')
-    book.id = "bookInfo"
+    let book = document.createElement('div');
+    book.id = "bookInfo";
+
+    // All Text
+    const allText = document.createElement('div');
+    allText.id = "allText";
+    book.append(allText);
+
+    //Top Field
+    const topField = document.createElement('div');
+    topField.id = "topField";
+    allText.append(topField);
+
+    // Book Title
     const bookTitle = document.createElement('span');
     bookTitle.id = "title";
-    const bookAuthor = document.createElement('span');
-    bookAuthor.id = "author";
+
+    // Year Published
     const bookPublished = document.createElement('span');
     bookPublished.id = "year";
-    const bookCover = document.createElement('span');
+
+    // Text field
+    const textField = document.createElement('div');
+    textField.id = "textField";
+    allText.appendChild(textField);
+
+    // Book Author
+    const bookAuthor = document.createElement('span');
+    bookAuthor.id = "author";
+
+    // Book Cover
+    const bookCover = document.createElement('div');
     bookCover.id = "cover";
+    const newImage = document.createElement('img');
+    newImage.id = "newImage";
+    newImage.alt = "Selected Book Cover"
+    bookCover.appendChild(newImage);
+
+    // Book Style
+    const bookCardMaker = (bookObj) => {
+        bookObj.style = `
+        border-style: solid;
+        border-right: transparent;
+        border-left: transparent;
+        border-top : transparent;
+        height: 250px
+        `
+
+        allText.style = `
+        position: absolute;
+        left: 225px;
+        margin-right: 100px;
+        `
+
+        title.style = `
+        font-weight: bold;
+        font-size : 30px;
+        `
+
+        year.style = `
+        `
+
+        author.style = `
+        font-size: 20px;
+        `
+
+        cover.style = `
+        float : left;
+        height: 240px;
+        `
+
+        newImage.style = `
+        height: 240px;
+        width : 160px;
+        position: absolute;
+        `
+    }
 
     // When Search Button is Clicked
     button.onclick = function () {
@@ -48,22 +116,23 @@ window.addEventListener('load', function () {
                 console.log(data.docs[0]);
 
                 // Gets information about the books
-                bookTitle.innerText = data.docs[0].title + " by: ";
+                bookTitle.innerText = data.docs[0].title;
                 bookAuthor.innerText = data.docs[0].author_name[0];
                 bookPublished.innerText = " (Published: " + data.docs[0].first_publish_year + ")";
 
-                book.appendChild(bookTitle);
-                book.appendChild(bookAuthor);
-                book.appendChild(bookPublished);
+                topField.appendChild(bookTitle);
+                textField.appendChild(bookAuthor);
+                topField.appendChild(bookPublished);
 
-                const newImage = document.createElement('img');
+
                 newImage.src = `https://covers.openlibrary.org/b/id/${data.docs[0].cover_i}-L.jpg`;
-                bookCover.appendChild(newImage);
+
 
                 book.appendChild(bookCover);
 
 
                 document.body.append(book);
+                bookCardMaker(book);
             })
             .catch(error => alert("Could not find a book with this name."));
 
@@ -73,10 +142,8 @@ window.addEventListener('load', function () {
         // .then(res => {return res.json()})
         // .then(data => console.log(data.Search));
     }
+
+
 });
 
-
-const cardMaker = (Obj) => {
-
-}
 
