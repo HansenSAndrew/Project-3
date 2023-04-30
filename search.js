2bbda0ded57b0feecadcba6729badd12d5113d6f
Andrew Hansen <123025570+HansenSAndrew@users.noreply.github.com>
2023-04-30 00:18:47 -0400

    Made multiple movies and book cards

--- search.js
+++ search.js
@@ -7,48 +7,93 @@ window.addEventListener('load', function () {
     console.log(button);
 
 
-    // Book Information
-    let book = document.createElement('div');
-    book.id = "bookInfo";
-
-    // All Text
-    const allText = document.createElement('div');
-    allText.id = "allText";
-    book.append(allText);
-
-    //Top Field
-    const topField = document.createElement('div');
-    topField.id = "topField";
-    allText.append(topField);
-
-    // Book Title
-    const bookTitle = document.createElement('span');
-    bookTitle.id = "title";
-
-    // Year Published
-    const bookPublished = document.createElement('span');
-    bookPublished.id = "year";
-
-    // Text field
-    const textField = document.createElement('div');
-    textField.id = "textField";
-    allText.appendChild(textField);
-
-    // Book Author
-    const bookAuthor = document.createElement('span');
-    bookAuthor.id = "author";
-
-    // Book Cover
-    const bookCover = document.createElement('div');
-    bookCover.id = "cover";
-    const newImage = document.createElement('img');
-    newImage.id = "newImage";
-    newImage.alt = "Selected Book Cover"
-    bookCover.appendChild(newImage);
 
     // Book Style
-    const bookCardMaker = (bookObj) => {
-        bookObj.style = `
+    const cardMaker = (mediaObj, isBook) => {
+
+        console.log(mediaObj);
+
+        // Cover Information
+        const mediaCover = document.createElement('div');
+        mediaCover.id = "info";
+
+        // All Text
+        const allText = document.createElement('div');
+        allText.id = "allText";
+        mediaCover.append(allText);
+
+        // Top Field
+        const topField = document.createElement('div');
+        topField.id = "topField";
+        allText.append(topField);
+
+        // Book Title
+        const title = document.createElement('span');
+        title.id = "title";
+        if (isBook) {
+            title.innerText = mediaObj.title;
+            topField.append(title);
+        }
+        else {
+            title.innerText = mediaObj.Title;
+            topField.append(title);
+        }
+
+        // Year Published
+        const published = document.createElement('span');
+        published.id = "year";
+        if (isBook) {
+            published.innerText = mediaObj.first_publish_year;
+            topField.append(published);
+        }
+        else {
+            published.innerText = mediaObj.Year;
+            topField.append(published);
+        }
+
+        // Text field
+        const textField = document.createElement('div');
+        textField.id = "textField";
+        allText.appendChild(textField);
+
+        // Book Author
+        if (isBook) {
+            const author = document.createElement('span');
+            author.id = "author";
+            author.innerText = mediaObj.author_name[0];
+            textField.appendChild(author);
+        }
+
+        // cover
+        const cover = document.createElement('div');
+        cover.id = "cover";
+        const newImage = document.createElement('img');
+        newImage.id = "newImage";
+        newImage.alt = "Selected Book Cover"
+        if (isBook) {
+            newImage.src = `https://covers.openlibrary.org/b/id/${mediaObj.cover_i}-L.jpg`;
+            cover.appendChild(newImage);
+            mediaCover.append(cover);
+        }
+        else {
+            newImage.src = mediaObj.Poster;
+            cover.appendChild(newImage);
+            mediaCover.append(cover);
+        }
+
+
+        mediaCover.style = `
+        position: static;
+        border-style: solid;
+        border-right: transparent;
+        border-left: transparent;
+        border-top: transparent;
+        height: 250px;
+        padding-top: 10px;
+        `
+
+        cover.style = `
+        position: static;
         border-style: solid;
         border-right: transparent;
         border-left: transparent;
@@ -67,9 +112,6 @@ window.addEventListener('load', function () {
         font-size : 30px;
         `
 
-        year.style = `
-        `
-
         author.style = `
         font-size: 20px;
         `
@@ -84,6 +126,10 @@ window.addEventListener('load', function () {
         width : 160px;
         position: absolute;
         `
+
+
+        let container = document.getElementById('results-container');
+        this.document.body.append(mediaCover);
     }
     // Event listener for the filter type dropdown for after results are displayed.
     this.document.getElementById('filter-results').onclick = function () {
@@ -144,39 +190,19 @@ window.addEventListener('load', function () {
         title = title.toLowerCase();
         console.log(title);
 
-        const bookPromise = fetch(`https://openlibrary.org/search.json?q=${title}`);
-        bookPromise
-            .then(res => { return res.json() })
-            .then(data => {
+        for (let i = 0; i < 3; i++) {
+            const bookPromise = fetch(`https://openlibrary.org/search.json?q=${title}`);
+            bookPromise
+                .then(res => { return res.json() })
+                .then(data => { cardMaker(data.docs[i], true) });
 
-                console.log(data.docs[0]);
-
-                // Gets information about the books
-                bookTitle.innerText = data.docs[0].title;
-                bookAuthor.innerText = data.docs[0].author_name[0];
-                bookPublished.innerText = " (Published: " + data.docs[0].first_publish_year + ")";
-
-                topField.appendChild(bookTitle);
-                textField.appendChild(bookAuthor);
-                topField.appendChild(bookPublished);
+            const moviePromise = fetch(`http://www.omdbapi.com/?s=${title}&apikey=6a9680f`);
+            moviePromise
+                .then(res => { return res.json() })
+                .then(data => { cardMaker(data.Search[i], false) });
 
 
-                newImage.src = `https://covers.openlibrary.org/b/id/${data.docs[0].cover_i}-L.jpg`;
-
-
-                book.appendChild(bookCover);
-
-                // Changed the append statement to append books to a result container instead of the body. Will be able to sort and update the results using this approach.
-                document.getElementById('results-container').append(book);
-                bookCardMaker(book);
-            })
-            .catch(error => alert("Could not find a book with this name."));
-
-
-        // const moviePromise = fetch(`http://www.omdbapi.com/?s=${title}&apikey=6a9680f`);
-        // moviePromise
-        // .then(res => {return res.json()})
-        // .then(data => console.log(data.Search));
+        }
     }
 
 
