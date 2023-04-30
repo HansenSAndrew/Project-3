4534c01d5c3ca8ac88662c9dc1c640a7a087d42c
TheSalmanK <salmankhattak2017@gmail.com>
2023-04-30 04:38:07 -0400

    Big update
    allows search based on all types of inputs. No longer has issues detecting movies vs books, all filter options work correctly and update the results list with the correct order and format. Added a specific function for making movie cards, could also be used for tvshows in the future. I think at this point we have all the major requirements for the project done.

--- search.js
+++ search.js
@@ -6,16 +6,94 @@ window.addEventListener('load', function () {
     const books = [];
     const movies = [];
     const shows = [];
-    const everything = [];
+    let everything = [];
 
     // Search Button
     const button = document.getElementById('searchButton');
     console.log(button);
+    // movieCardMaker, needs to be implemented, only way I could think of to have the filter work correctly.
+    const movieCardMaker = (mediaObj, isMovie) => {
+        console.log(mediaObj);
 
+        // Cover Information
+        const mediaCover = document.createElement('div');
+        mediaCover.id = "info";
 
+        // All Text
+        const allText = document.createElement('div');
+        allText.id = "allText";
+        mediaCover.append(allText);
 
-    // Book Style
-    const cardMaker = (mediaObj, isBook) => {
+        // Top Field
+        const topField = document.createElement('div');
+        topField.id = "topField";
+        allText.append(topField);
+
+        // Movie Title
+        const title = document.createElement('span');
+        title.id = "title";
+        if (isMovie) {
+            title.innerText = mediaObj.Title;
+            topField.append(title);
+        }
+        else {
+            title.innerText = mediaObj.Title;
+            topField.append(title);
+        }
+
+        // Year Published
+        const published = document.createElement('span');
+        published.id = "year";
+        if (isMovie) {
+            published.innerText = mediaObj.Year;
+            topField.append(published);
+        }
+        else {
+            published.innerText = mediaObj.Year;
+            topField.append(published);
+        }
+
+        // Text field
+        const textField = document.createElement('div');
+        textField.id = "textField";
+        allText.appendChild(textField);
+
+        // Movie Director
+        if (isMovie) {
+            const director = document.createElement('span');
+            director.id = "director";
+            director.innerText = mediaObj.Director;
+            textField.appendChild(director);
+        }
+
+        // cover
+        const cover = document.createElement('div');
+        cover.id = "cover";
+        const newImage = document.createElement('img');
+        newImage.id = "newImage";
+        newImage.alt = "Selected Movie Cover"
+        if (isMovie) {
+            newImage.src = mediaObj.Poster;
+            cover.appendChild(newImage);
+            mediaCover.append(cover);
+        }
+        else {
+            newImage.src = mediaObj.Poster;
+            cover.appendChild(newImage);
+            mediaCover.append(cover);
+        }
+        let container = document.getElementById('results-container');
+        container.appendChild(mediaCover);
+        // if the movies array doesnt have the mediaObj, push it to the array.
+        if (!movies.includes(mediaObj)) {
+            movies.push(mediaObj);
+            console.log("movies:");
+            console.log(movies);
+        }
+    }
+
+    // renamed this to bookCardMaker for functionality.
+    const bookCardMaker = (mediaObj, isBook) => {
 
         console.log(mediaObj);
 
@@ -136,12 +214,70 @@ window.addEventListener('load', function () {
 
         let container = document.getElementById('results-container');
         container.appendChild(mediaCover);
+        // if the books array doesnt have the mediaObj, push it to the array.
+        if (!books.includes(mediaObj)) {
+            books.push(mediaObj);
+            console.log("books:");
+            console.log(books);
+        }
     }
+
     // Event listener for the filter type dropdown for after results are displayed.
     this.document.getElementById('filter-results').onclick = function () {
         const filterValue = document.getElementById('filter-results').value;
+        everything = [...books, ...movies, ...shows];
+        console.log("Everything");
+        console.log(everything);
         console.log(filterValue);
+        if (filterValue == "alphabetical") {
+            // filter the books array by title, then send them through the card maker.
+            books.sort(function (a, b) {
+                if (a.title < b.title) { return -1; }
+                if (a.title > b.title) { return 1; }
+                return 0;
+            });
+        }
+        else if (filterValue == "oldest") {
+            // filter the results from the oldest to newest.
+            books.sort(function (a, b) {
+                if (a.first_publish_year < b.first_publish_year) { return -1; }
+                if (a.first_publish_year > b.first_publish_year) { return 1; }
+                return 0;
+            }
+            );
+        }
+        else if (filterValue == "newest") {
+            // filter the results from the newest to oldest.
+            books.sort(function (a, b) {
+                if (a.first_publish_year > b.first_publish_year) { return -1; }
+                if (a.first_publish_year < b.first_publish_year) { return 1; }
+                return 0;
+            }
+            );
+        }
+        else if (filterValue == "reverse-alphabetical") {
+            // filter the results from the newest to oldest.
+            books.sort(function (a, b) {
+                if (a.title > b.title) { return -1; }
+                if (a.title < b.title) { return 1; }
+                return 0;
+            }
+            );
+        }
+        let book = false;
+        document.getElementById("results-container").innerHTML = "";
+        for (let i = 0; i < everything.length; i++) {
+            // check if everything[i] contains an author field.
+            if (everything[i].hasOwnProperty("author_name")) {
+                book = true;
+                bookCardMaker(everything[i], true);
+            } else {
+                movieCardMaker(everything[i], true);
+            }
+        }
     }
+
+
 
     // When Search Button is Clicked
     button.onclick = function () {
@@ -154,6 +290,7 @@ window.addEventListener('load', function () {
         let authorFlag = false;
         let startDateFlag = false;
         let endDateFlag = false;
+        let sameYear = false;
 
 
         // Collecting all potential search values from the search criteria.
@@ -184,7 +321,10 @@ window.addEventListener('load', function () {
 
         }
         if (document.getElementById('end-year').value != "" && document.getElementById('start-year').value != "" &&
-            document.getElementById('end-year').value > document.getElementById('start-year').value) {
+            document.getElementById('end-year').value >= document.getElementById('start-year').value) {
+            if (document.getElementById('start-year').value == document.getElementById('end-year').value) {
+                sameYear = true;
+            }
             endDateFlag = true;
             console.log("End Year: " + endDateValue);
             // implement search type here
@@ -212,53 +352,71 @@ window.addEventListener('load', function () {
             title = title.toLowerCase();
             console.log(title);
             let link = 'https://openlibrary.org/search.json?q=';
-            bookSearch(title);
+            bookSearch(title, link);
+            movieSearch(title);
 
         }
-        // if (authorFlag == true) {
-        //     // searching for book authors
-        //     const splitSearch = authorValue.split(" ");
-        //     console.log(splitSearch);
+        if (authorFlag == true) {
+            // searching for book authors
+            const splitSearch = authorValue.split(" ");
+            console.log(splitSearch);
 
-        //     // Format Title for API Search
-        //     let author = "";
-        //     for (let i = 0; i < splitSearch.length; i++) {
-        //         author += splitSearch[i];
-        //         if (i < splitSearch.length - 1)
-        //             author += "+"
-        //     }
-        //     author = author.toLowerCase();
-        //     console.log(author);
-        //     let link = 'https://openlibrary.org/search.json?author=';
-        //     bookSearch(author, link);
+            // Format Title for API Search
+            let author = "";
+            for (let i = 0; i < splitSearch.length; i++) {
+                author += splitSearch[i];
+                if (i < splitSearch.length - 1)
+                    author += "+"
+            }
+            author = author.toLowerCase();
+            console.log(author);
+            let link = 'https://openlibrary.org/search.json?author=';
+            bookSearch(author, link);
 
-        // }
-        // if (startDateFlag == true && endDateFlag == true) {
-        //     // Format Title for API Search
-        //     let dates = `[${startDateValue}+TO+${endDateValue}]`;
-        //     console.log("Dates in the search: " + dates);
-        //     let link = 'https://openlibrary.org/search.json?q=first_publish_year%3A';
-        //     bookSearch(dates, link);
-        // }
+        }
+        if (startDateFlag == true && endDateFlag == true) {
+            // Format Title for API Search
+            let dates = `[${startDateValue}+TO+${endDateValue}]`;
+            console.log("Dates in the search: " + dates);
+            let link = 'https://openlibrary.org/search.json?q=first_publish_year%3A';
+            bookSearch(dates, link);
+            if (sameYear) {
+                movieYearSearch(startDateValue);
+            }
+        }
 
 
     }
 
     // Function to search for books, takes in the link and the data to search for.
-    const bookSearch = (title) => {
+    const bookSearch = (title, link) => {
 
         for (let i = 0; i < 3; i++) {
-            const bookPromise = fetch(`https://openlibrary.org/search.json?q=${title}`);
+            const bookPromise = fetch(`${link}${title}`);
             bookPromise
                 .then(res => { return res.json() })
-                .then(data => { cardMaker(data.docs[i], true) });
+                .then(data => { bookCardMaker(data.docs[i], true) });
 
+        }
+    }
+
+    const movieSearch = title => {
+        for (let i = 0; i < 3; i++) {
             const moviePromise = fetch(`http://www.omdbapi.com/?s=${title}&apikey=6a9680f`);
             moviePromise
                 .then(res => { return res.json() })
-                .then(data => { cardMaker(data.Search[i], false) });
+                .then(data => { movieCardMaker(data.Search[i], true) });
+        }
+    }
 
 
+    // Broken function... API doesn't want to work with this.
+    const movieYearSearch = (year) => {
+        for (let i = 0; i < 3; i++) {
+            const moviePromise = fetch(`http://www.omdbapi.com/?y=${year}&apikey=6a9680f`);
+            moviePromise
+                .then(res => { return res.json() })
+                .then(data => { movieCardMaker(data.Search, true) });
         }
     }
 
