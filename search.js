c2f33a0464e0db9d46f8b94b43f1b290d367d0f1
TheSalmanK <salmankhattak2017@gmail.com>
2023-04-29 23:23:08 -0400

    updated book searching to include books from date ranges, authors, and titles.

--- search.js
+++ search.js
@@ -93,6 +93,14 @@ window.addEventListener('load', function () {
 
     // When Search Button is Clicked
     button.onclick = function () {
+        // flags for the search:
+
+        let titleFlag = false;
+        let authorFlag = false;
+        let startDateFlag = false;
+        let endDateFlag = false;
+
+
         // Collecting all potential search values from the search criteria.
         const titleValue = document.getElementById('title').value;
         const authorValue = document.getElementById('author').value;
@@ -103,21 +111,26 @@ window.addEventListener('load', function () {
 
         // console logging all search values to make sure nothings weird. We should use these as starting points for what searches we want to do.
         if (document.getElementById('title').value != "") {
+            titleFlag = true;
             console.log("Title: " + titleValue);
             // implement search type here
 
         }
         if (document.getElementById('author').value != "") {
+            authorFlag = true;
             console.log("Author: " + authorValue);
             // implement search type here
 
         }
         if (document.getElementById('start-year').value != "") {
+            startDateFlag = true;
             console.log("Start Year: " + startDateValue);
             // implement search type here
 
         }
-        if (document.getElementById('end-year').value != "" && document.getElementById('start-year').value != "" && document.getElementById('end-year').value > document.getElementById('start-year').value) {
+        if (document.getElementById('end-year').value != "" && document.getElementById('start-year').value != "" &&
+            document.getElementById('end-year').value > document.getElementById('start-year').value) {
+            endDateFlag = true;
             console.log("End Year: " + endDateValue);
             // implement search type here
         }
@@ -127,9 +140,10 @@ window.addEventListener('load', function () {
 
         }
 
+        // handling the search based on the flags.
 
-
-        // Splits String
+        if (titleFlag == true) {
+        // Searching book titles
         const splitSearch = titleValue.split(" ");
         console.log(splitSearch);
 
@@ -140,11 +154,50 @@ window.addEventListener('load', function () {
             if (i < splitSearch.length - 1)
                 title += "+"
         }
-
         title = title.toLowerCase();
-        console.log(title);
+            console.log(title);
+            let link = 'https://openlibrary.org/search.json?q=';
+            bookSearch(link, title);
 
-        const bookPromise = fetch(`https://openlibrary.org/search.json?q=${title}`);
+        }
+        if (authorFlag == true) {
+            // searching for book authors
+            const splitSearch = authorValue.split(" ");
+        console.log(splitSearch);
+
+        // Format Title for API Search
+        let author = "";
+        for (let i = 0; i < splitSearch.length; i++) {
+            author += splitSearch[i];
+            if (i < splitSearch.length - 1)
+                author += "+"
+        }
+        author = author.toLowerCase();
+            console.log(author);
+            let link = 'https://openlibrary.org/search.json?author=';
+            bookSearch(link, author);
+
+        }
+        if (startDateFlag == true && endDateFlag == true) {
+            const splitSearch = authorValue.split(" ");
+        console.log(splitSearch);
+
+        // Format Title for API Search
+        let dates = `[${startDateValue}TO${endDateValue}]`;
+            console.log(dates);
+            let link = 'https://openlibrary.org/search.json?q=first_publish_year';
+            bookSearch(link, dates);
+        }
+        // const moviePromise = fetch(`http://www.omdbapi.com/?s=${title}&apikey=6a9680f`);
+        // moviePromise
+        // .then(res => {return res.json()})
+        // .then(data => console.log(data.Search));
+    }
+
+    // Function to search for books, takes in the link and the data to search for.
+    const bookSearch = (data, link) => {
+
+        const bookPromise = fetch(`${link}${data}`);
         bookPromise
             .then(res => { return res.json() })
             .then(data => {
@@ -171,12 +224,6 @@ window.addEventListener('load', function () {
                 bookCardMaker(book);
             })
             .catch(error => alert("Could not find a book with this name."));
-
-
-        // const moviePromise = fetch(`http://www.omdbapi.com/?s=${title}&apikey=6a9680f`);
-        // moviePromise
-        // .then(res => {return res.json()})
-        // .then(data => console.log(data.Search));
     }
 
 
