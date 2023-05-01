c04f03359f8814b2de697e0152b913443df86810
TheSalmanK <salmankhattak2017@gmail.com>
2023-05-01 18:42:01 -0400

    more pretty stuff
    sd

--- search.js
+++ search.js
@@ -18,134 +18,137 @@ window.addEventListener('load', function () {
     // movieCardMaker, needs to be implemented, only way I could think of to have the filter work correctly.
     const movieCardMaker = (mediaObj) => {
 
-        // Cover Information
-        const mediaCover = document.createElement('div');
-        mediaCover.id = "info";
+    // Cover Information
+    const mediaCover = document.createElement('div');
+    mediaCover.id = "info";
 
-        // All Text
-        const allText = document.createElement('div');
-        allText.id = "allText";
-        mediaCover.append(allText);
+    // Movie Poster (cover)
+    const cover = document.createElement('div');
+    cover.id = "cover";
+    const newImage = document.createElement('img');
+    newImage.id = "newImage";
+    newImage.alt = "Selected Movie Cover";
+    newImage.src = mediaObj.Poster;
+    cover.appendChild(newImage);
+    mediaCover.append(cover);
 
-        // Top Field
-        const topField = document.createElement('div');
-        topField.id = "topField";
-        allText.append(topField);
+    // All Text
+    const allText = document.createElement('div');
+    allText.id = "allText";
+    mediaCover.append(allText);
 
-        // Movie Title
-        const title = document.createElement('span');
-        title.id = "title";
-        title.innerText = mediaObj.Title;
-        topField.append(title);
+    // Top Field
+    const topField = document.createElement('div');
+    topField.id = "topField";
+    allText.append(topField);
 
-        // Year Published
-        const published = document.createElement('span');
-        published.id = "year";
-        published.innerText = mediaObj.Year;
-        topField.append(published);
+    // Movie Title
+    const title = document.createElement('span');
+    title.id = "title";
+    title.innerText = mediaObj.Title;
+    topField.append(title);
 
+    // Year Published
+    const published = document.createElement('span');
+    published.id = "year";
+    published.innerText = mediaObj.Year;
+    topField.append(published);
 
-        // Text field
-        const textField = document.createElement('div');
-        textField.id = "textField";
-        allText.appendChild(textField);
+    // Text field
+    const textField = document.createElement('div');
+    textField.id = "textField";
+    allText.appendChild(textField);
 
-        // Movie Poster (cover)
-        const cover = document.createElement('div');
-        cover.id = "cover";
-        const newImage = document.createElement('img');
-        newImage.id = "newImage";
-        newImage.alt = "Selected Movie Cover"
-        newImage.src = mediaObj.Poster;
-        cover.appendChild(newImage);
-        mediaCover.append(cover);
+    // Stylizes Card
+    stylize(mediaCover);
 
-        // Stylizes Card
-        stylize(mediaCover);
-
-        let container = document.getElementById('results-container');
-        container.appendChild(mediaCover);
-        // if the movies array doesnt have the mediaObj, push it to the array.
-        if (!movies.includes(mediaObj)) {
-            movies.push(mediaObj);
-            console.log("movies:");
-            console.log(movies);
-        }
+    let container = document.getElementById('results-container');
+    container.appendChild(mediaCover);
+    // if the movies array doesnt have the mediaObj, push it to the array.
+    if (!movies.includes(mediaObj)) {
+        movies.push(mediaObj);
+        console.log("movies:");
+        console.log(movies);
     }
+}
 
-    // renamed this to bookCardMaker for functionality.
-    const bookCardMaker = (mediaObj) => {
+const bookCardMaker = (mediaObj) => {
 
-        // Cover Information
-        const mediaCover = document.createElement('div');
-        mediaCover.id = "info";
+    // Cover Information
+    const mediaCover = document.createElement('div');
+    mediaCover.id = "info";
 
-        // All Text
-        const allText = document.createElement('div');
-        allText.id = "allText";
-        mediaCover.append(allText);
+    // cover
+    const cover = document.createElement('div');
+    cover.id = "cover";
+    const newImage = document.createElement('img');
+    newImage.id = "newImage";
+    newImage.alt = "Selected Book Cover";
+    newImage.src = `https://covers.openlibrary.org/b/id/${mediaObj.cover_i}-L.jpg`;
+    cover.appendChild(newImage);
+    mediaCover.append(cover);
 
-        // Top Field
-        const topField = document.createElement('div');
-        topField.id = "topField";
-        allText.append(topField);
+    // All Text
+    const allText = document.createElement('div');
+    allText.id = "allText";
+    mediaCover.append(allText);
 
-        // Book Title
-        const title = document.createElement('span');
-        title.id = "title";
-        title.innerText = mediaObj.title;
-        topField.append(title);
+    // Top Field
+    const topField = document.createElement('div');
+    topField.id = "topField";
+    allText.append(topField);
 
-        // Year Published
-        const published = document.createElement('span');
-        published.id = "year";
-        published.innerText = mediaObj.first_publish_year;
-        topField.append(published);
+    // Book Title
+    const title = document.createElement('span');
+    title.id = "title";
+    title.innerText = mediaObj.title;
+    topField.append(title);
 
-        // Text field
-        const textField = document.createElement('div');
-        textField.id = "textField";
-        allText.appendChild(textField);
+    // Year Published
+    const published = document.createElement('span');
+    published.id = "year";
+    published.innerText = mediaObj.first_publish_year;
+    topField.append(published);
 
-        // Book Author
-        const author = document.createElement('span');
-        author.id = "author";
-        author.innerText = mediaObj.author_name[0];
-        textField.appendChild(author);
+    // Text field
+    const textField = document.createElement('div');
+    textField.id = "textField";
+    allText.appendChild(textField);
 
-        // cover
-        const cover = document.createElement('div');
-        cover.id = "cover";
-        const newImage = document.createElement('img');
-        newImage.id = "newImage";
-        newImage.alt = "Selected Book Cover"
-        newImage.src = `https://covers.openlibrary.org/b/id/${mediaObj.cover_i}-L.jpg`;
-        cover.appendChild(newImage);
-        mediaCover.append(cover);
-
-        // Stylizes card
-        stylize(mediaCover);
-
-        // Seperate style for just books
-        author.style = `
-        font-size: 20px;
+    // Book Author
+    const author = document.createElement('span');
+    author.id = "author";
+    author.innerText = mediaObj.author_name[0];
+    textField.appendChild(author);
+    author.style = `
+        font-size: 16px;
+        color: #ffffff;
+        height: 20px;
+        text-shadow: 0px 2px 5px #00000085;
         `
 
-        let container = document.getElementById('results-container');
-        container.appendChild(mediaCover);
-        // if the books array doesnt have the mediaObj, push it to the array.
-        if (!books.includes(mediaObj)) {
-            books.push(mediaObj);
-            console.log("books:");
-            console.log(books);
-        }
+    // Stylizes card
+    stylize(mediaCover);
+
+
+    let container = document.getElementById('results-container');
+    container.appendChild(mediaCover);
+    // if the books array doesnt have the mediaObj, push it to the array.
+    if (!books.includes(mediaObj)) {
+        books.push(mediaObj);
+        console.log("books:");
+        console.log(books);
     }
+}
+
+
 
     // This function is used to stylize cards (results)
     function stylize(mediaCover) {
 
-        const allText = mediaCover.childNodes[0];
-        const cover = mediaCover.childNodes[1];
+        const cover = mediaCover.childNodes[0];
+        const allText = mediaCover.childNodes[1];
+;
 
         const topField = allText.childNodes[0];
         const textField = allText.childNodes[1];
@@ -157,45 +160,62 @@ window.addEventListener('load', function () {
 
         const newImage = cover.childNodes[0];
 
+        topField.style = `
+        width: 500px;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        `
+
         mediaCover.style = `
-        position: static;
+        display: flex;
+        flex-direction: row;
         border-style: solid;
         border-right: transparent;
         border-left: transparent;
         border-top: transparent;
-        height: 250px;
-        padding-top: 10px;
+        border-color: #ffffff;
+        height: 230px;
+        width: 700px;
+        margin-top: 50px;
         `
-
         cover.style = `
-        position: static;
-        border-style: solid;
-        border-right: transparent;
-        border-left: transparent;
-        border-top : transparent;
-        height: 250px
+        height: 200px;
         `
 
+        newImage.style = `
+        height: 200px;
+        width : 133px;
+        object-fit: cover;
+        padding-right: 70px;
+        `
+        cover.style = `
+        height: 200px
+        `
         allText.style = `
-        position: absolute;
-        left: 225px;
         margin-right: 100px;
+        font-family: 'Roboto', sans-serif;
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+        `
+        year.style = `
+        font-size: 20px;
+        color: #ffffff;
+        margin-left: 10px;
+        border-left: solid;
+        border-color: #ffffff;
+        border-width: 1px;
+        padding-left: 10px;
+        height: 20px;
+        text-shadow: 0px 2px 5px #00000085;
         `
 
         title.style = `
         font-weight: bold;
-        font-size : 30px;
-        `
-
-        cover.style = `
-        float : left;
-        height: 240px;
-        `
-
-        newImage.style = `
-        height: 240px;
-        width : 160px;
-        position: absolute;
+        color: #ffffff;
+        font-size : 20px;
+        text-shadow: 0px 2px 5px #00000085;
         `
     }
 
@@ -205,16 +225,16 @@ window.addEventListener('load', function () {
 
     // helper method to extract title and year from an item.
     function getTitleAndYear(item) {
-    let title, year;
-    if (item.hasOwnProperty("author_name")) { // book
-        title = item.title;
-        year = item.first_publish_year;
-    } else { // movie or show
-        title = item.Title;
-        year = item.Year;
+        let title, year;
+        if (item.hasOwnProperty("author_name")) { // book
+            title = item.title;
+            year = item.first_publish_year;
+        } else { // movie or show
+            title = item.Title;
+            year = item.Year;
+        }
+        return { title, year };
     }
-    return { title, year };
-}
 
 
     // Event listener for the filter type dropdown for after results are displayed.
@@ -291,6 +311,7 @@ window.addEventListener('load', function () {
     // When Search Button is Clicked
     button.onclick = function () {
         document.getElementById("results-container").innerHTML = "";
+        
 
 
         // flags for the search:
@@ -303,20 +324,20 @@ window.addEventListener('load', function () {
 
 
         // Collecting all potential search values from the search criteria.
-        const titleValue = document.getElementById('title').value;
-        const authorValue = document.getElementById('author').value;
+        const titleValue = document.getElementById('title-input').value;
+        const authorValue = document.getElementById('author-input').value;
         const startDateValue = document.getElementById('start-year').value;
         const endDateValue = document.getElementById('end-year').value;
 
 
         // console logging all search values to make sure nothings weird. We should use these as starting points for what searches we want to do.
-        if (document.getElementById('title').value != "") {
+        if (document.getElementById('title-input').value != "") {
             titleFlag = true;
             console.log("Title: " + titleValue);
             // implement search type here
 
         }
-        if (document.getElementById('author').value != "") {
+        if (document.getElementById('author-input').value != "") {
             authorFlag = true;
             console.log("Author: " + authorValue);
             // implement search type here
